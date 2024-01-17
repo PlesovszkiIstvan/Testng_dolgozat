422282e18a4ac0e53dc80fe5c616a46aa868c4a4
PlesovszkiIstvan <kilot58@gmail.com>
2024-01-17 08:50:30 +0100

    terület test

--- test/test.java
+++ test/test.java
@@ -10,4 +10,18 @@ public class test {
         double actual = 765.8131720807066;
         Assert.assertEquals(calculated, actual);
     }
+    @Test
+    public void testCalculateAreaFail(){
+        Calculate calc = new Calculate();
+        double calculated = calc.calcArea(30, 45);
+        double actual = 5;
+        Assert.assertEquals(calculated, actual);
+    }
+    @Test
+    public void testCalculateDiameter(){
+        Calculate calculate = new Calculate();
+        double calculated = calc.calcDiameter(30);
+        double actual = 120;
+        Assert.assertEquals(calculated, actual);
+    }
 }
