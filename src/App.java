0ecd5b5881795787df91bc5925764ae5880293dc
PlesovszkiIstvan <kilot58@gmail.com>
2024-01-17 09:07:58 +0100

    kesz ugyahogy

--- src/App.java
+++ src/App.java
@@ -12,10 +12,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Calculate calculate = new Calculate();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Kérek egy Alpha szöget!");
         double alpha = scanner.nextDouble();
         System.out.println("Kérem az oldal hosszát!");
         double side = scanner.nextDouble();
+        System.out.println("Terület: " + calculate.calcArea(side, alpha));
+        System.out.println("Kerület: " + calculate.calcDiameter(side));
     }
 }
